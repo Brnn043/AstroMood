ad1f2e2ba98b2a0c8285d0279e2b15a75819ca43
Brnn <95439638+Brnn043@users.noreply.github.com>
2025-01-06 23:54:39 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
         <div className="absolute inset-0 z-0"><Image src={"/bg.png"} alt={"bg"} layout="fill" objectFit="cover" /></div>
         <div className="absolute inset-0 z-1 bg-black/30"></div>
         <div className="absolute inset-0 opacity-10 animate-customPulse">
-            <Image src={"/galaxy.png"} alt={"galaxy"} layout="fill" objectFit="fill" />
+            <Image src={"/galaxy.PNG"} alt={"galaxy"} layout="fill" objectFit="fill" />
         </div>
         <div className="relative text-white h-full w-full flex justify-center items-center content-center bg-cover overflow-hidden">
           {children}
